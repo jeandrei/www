487f8a6728eb9a805f5d75ce120cb1e387750904
Jeandrei <jeandrei@gmail.com>
2018-01-03 21:38:20 -0200

    atualização

--- dev/classes/Controller.php
+++ dev/classes/Controller.php
@@ -13,7 +13,10 @@ abstract class Controller{
 	}//executeAction
 
 	protected function returnView($viewmodel, $fullview){
-		$view = 'views/'. get_class($this). '/' . $this->action. '.php';
+		$view = 'views/'.strtolower(get_class($this)). '/' . $this->action. '.php';
+		//vai montar o seguinte em view **views/Home/index.php** Home é o nome da classe que sempre
+		//colocamos em letra maiúscula mas a pasta é com nome em letra minúscula logo 
+		//transformo tudo em letra minúscula com o strtolower para ficar **views/home/index.php**
 		if($fullview){
 			require('views/main.php');
 		}else {
