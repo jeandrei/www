5b462dbcdebcb638313340acdd7f990b4db4bd1e
Jeandrei <jeandrei@gmail.com>
2018-01-03 23:42:25 -0200

    atualização

--- dev/views/home/index.php
+++ dev/views/home/index.php
@@ -1,1 +1,5 @@
-This is home/index
+<div class="text-center">
+	<h1>Welcome To ShareBoard</h1>
+	<p class="lead">Find something cool? Share with our community. Look at other shares ass well.</p>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Share Now</a>
+</div>
--- dev/views/main.php
+++ dev/views/main.php
@@ -2,8 +2,50 @@
 <html>
 <head>
 	<title>Sharebord</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
-	<?php require($view); ?>
+<body>   
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>shares">Shares <span class="sr-only">(current)</span></a>
+          </li> 
+        </ul>
+
+        <ul class="navbar-nav mr-sm-2">
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>users/login">Login <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>users/register">Register <span class="sr-only">(current)</span></a>
+          </li> 
+        </ul>
+        <!--
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>-->
+    </nav>
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead"><?php require($view) ;?></p>
+      </div>
+
+    </main><!-- /.container -->
+
 </body>
 </html>
