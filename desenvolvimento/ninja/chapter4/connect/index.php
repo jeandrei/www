7356479f2b5013f6b7052971d5477a7b0b0a105d
Jeandrei <jeandreiwalter@gmail.com>
2018-07-27 08:49:58 -0300

    Nome container

--- desenvolvimento/ninja/chapter4/connect/index.php
+++ desenvolvimento/ninja/chapter4/connect/index.php
@@ -21,7 +21,7 @@ exit();
 try
 {
 	//www_db_1 e o nome do container no docker
-	$pdo = new PDO('mysql:host=www_db_1;dbname=ijdb','root','rootadm');
+	$pdo = new PDO('mysql:host=db;dbname=ijdb','root','rootadm');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$pdo->exec('SET NAMES "utf8"');
 }
