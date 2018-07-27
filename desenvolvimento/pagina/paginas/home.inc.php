094d7cc5a894d9805774972caef20e148816cf3e
Jeandrei <jeandreiwalter@gmail.com>
2018-07-27 15:48:02 -0300

    pagina blocos seduc

--- desenvolvimento/pagina/paginas/home.inc.php
+++ desenvolvimento/pagina/paginas/home.inc.php
@@ -42,45 +42,7 @@
 				</p>
 			</section><!--fim esquerdo b1-->
 
-			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
-				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
-				</div>
-				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
-				</p>
-			</section><!--fim esquerdo b1-->
-
-			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
-				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
-				</div>
-				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
-				</p>
-			</section><!--fim esquerdo b1-->
-
-			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
-				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
-				</div>
-				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
-				</p>
-			</section><!--fim esquerdo b1-->
-
-			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
-				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
-				</div>
-				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
-				</p>
-			</section><!--fim esquerdo b1-->
+			
 			
 		
 	</section><!--section principal-->
@@ -98,16 +60,7 @@
 					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
 				</p>
 		</section><!--fim esquerdo b1-->
-
-		<section class="box_v">				
-				<h1>Instalação do TeamViwer</h1>
-				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
-				</div>
-				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
-				</p>
-		</section><!--fim esquerdo b1-->
+		
 
 	</section><!--fim esquerdo b1-->
 			
