d761d41717da3df8fd709f68ca3f59e8004372e9
Jeandrei <jeandreiwalter@gmail.com>
2018-07-31 23:10:15 -0300

    atualizacao

--- desenvolvimento/pagina/cabecalho.php
+++ desenvolvimento/pagina/cabecalho.php
@@ -2,6 +2,18 @@
 		<header class="banner">
 			
 			<img src="imagens/banner2.png">
+			<div style="
+						position: absolute;
+						margin-top: -90px;
+						font-weight: bold;
+						font-style: italic;
+						font-size: 30px;
+						font-family: Arial, Helvetica, sans-serif;	
+						color: white;
+						margin-left: 20px;
+			">
+				SUPORTE
+			</div>
 
 		</header>
 	<!--******************************************Fim Cabeçalho do site***************************************-->
