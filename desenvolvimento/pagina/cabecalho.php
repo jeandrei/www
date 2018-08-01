f9ab6d08d8f3d805203cdecffd4ccfc3fd2419dd
Jeandrei <jeandreiwalter@gmail.com>
2018-07-31 22:56:51 -0300

    atualizacao

--- desenvolvimento/pagina/cabecalho.php
+++ desenvolvimento/pagina/cabecalho.php
@@ -1,7 +1,7 @@
 <!--******************************************02 Cabeçalho do site******************************************-->
 		<header class="banner">
-			<h1>JEANDREI WALTER</h1>
-			<p>Bem vindo ao site pessoal de Jeandrei Walter</p>
-			<p>Aqui você encontra conteúdos diversos relacionados as tecnologias.
+			
+			<img src="imagens/banner2.png">
+
 		</header>
 	<!--******************************************Fim Cabeçalho do site***************************************-->
