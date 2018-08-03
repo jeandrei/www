6b2a100448b16fe4768e4a32596087ff6060838e
Jeandrei <jeandreiwalter@gmail.com>
2018-08-03 14:08:40 -0300

    paginas dropbox quadro horarios manuais

--- desenvolvimento/pagina/paginas/home.inc.php
+++ desenvolvimento/pagina/paginas/home.inc.php
@@ -13,32 +13,33 @@
 			</section><!--fim esquerdo b1-->
 
 			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
+				<h1>Instalação do Dropbox</h1>
 				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
+					<img src="imagens\dropbox\icone.png">
 				</div>
 				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
+					Saiba como instalar o Dropbox e manter seus arquivos seguros em nuvem. <a href="index.php?p=dropbox">Clique aqui!</a>
 				</p>
 			</section><!--fim esquerdo b1-->
 
 			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
+				<h1>Planilha de Quadro de Horários</h1>
 				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
+					<img src="imagens\quadrohorarios\icone.png">
 				</div>
 				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
+					Baixe a planilha de quadro de horários e aprenda como usá-la.
+					Com esta planilha fica mais fácil montar seu quadro de horários.<a href="index.php?p=quadrohorarios">Clique aqui!</a>
 				</p>
 			</section><!--fim esquerdo b1-->
 
 			<section class="box">				
-				<h1>Instalação do TeamViwer</h1>
+				<h1>Manuais</h1>
 				<div id="imagem">
-					<img src="imagens\teamviwer.jpg">
+					<img src="imagens\manuais\icone.png">
 				</div>
 				<p>							
-					Saiba como instalar o TeamViwer a fim de obter suporte remoto. <a href="#">Clique aqui!</a>
+					Baixe os manuais de apoio. <a href="index.php?p=manuais">Clique aqui!</a>
 				</p>
 			</section><!--fim esquerdo b1-->
 
---
+++ desenvolvimento/pagina/paginas/quadrohorarios.inc.php
@@ -0,0 +1,58 @@
+<!--********************05 PARTE PRINCIPAL DO SITE****************-->
+	<section class="sectionPaginas">
+
+		<section class="box_full">
+			<h1>Instalação do Dropbox</h1>
+			<p>
+				<ol style="margin-left: 20px;">
+					<li>
+						Baixe o Dropbox&nbsp;<a href="https://www.dropbox.com/pt_BR/downloading">aqui.</a>
+					</li>
+
+					<br>
+
+					<li>
+						Selecione a pasta para o Download e clique em Salvar.
+						<br>
+						<img src="imagens\dropbox\01.png">
+					</li>
+
+					<br>
+
+					<li>
+						Após finalizar o download execute o instalador do Dropbox.
+						<br>
+						<img src="imagens\dropbox\02.png">
+					</li>
+
+					<br>
+
+					<li>
+						Aguarde até a conclusão da instalação do Dropbox.
+						<br>
+						<img src="imagens\dropbox\03.png">
+					</li>
+
+					<br>
+
+					<li>
+						Na janela seguinte informe o E-mail e senha cadastrado na sua conta Dropbox.<br>
+						Ao final clique em Acessar sua conta
+						<br>
+						<img src="imagens\dropbox\04.png">
+					</li>
+
+					<br>
+
+					<li>
+						Ao final o Team Viwer será executado automaticamente e criará um atalho na área de trabalho.
+						<br>
+						Sempre que necessário solicitar suporte informe Sua ID e Senha como indicado.
+						<br>
+						<img src="imagens\teamviwer\05.png" style="width: 900px; height: 500px">
+					</li>
+				</ol>
+			</p>
+		</section><!--fim bloco 1-->	
+		
+	</section>
