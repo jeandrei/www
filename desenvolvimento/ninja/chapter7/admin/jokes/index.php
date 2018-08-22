9bb69a2143807b81a4886b4bfb0a674c1135ed90
Jeandrei <jeandreiwalter@gmail.com>
2018-08-21 21:55:51 -0300

    pagina 216

--- desenvolvimento/ninja/chapter7/admin/jokes/index.php
+++ desenvolvimento/ninja/chapter7/admin/jokes/index.php
@@ -35,6 +35,16 @@ foreach ($result as $row)
 	$categories[] = array('id' => $row['id'], 'name' => $row['name']);
 }
 
+if(isset($_GET['action']) and $_GET['action'] == 'search')
+{
+	include INCLUDES . '/db.inc.php';
+	//The basic statement	
+	$select = 'SELECT id, joketext';
+	$from = 'FROM joke';
+	$where = 'WHERE TRUE';
+	echo "parei pagina 216";
+	exit();
+}
 
 include 'searchform.html.php';
 
--- desenvolvimento/ninja/chapter7/admin/jokes/searchform.html.php
+++ desenvolvimento/ninja/chapter7/admin/jokes/searchform.html.php
@@ -7,13 +7,18 @@ include INCLUDES . '/helpers.inc.php';
 <head>
 	<meta charset="utf-8">
 	<title>Manage jokes</title>
+	<script src="../../funcoes/javascript.js"></script>
 </head>
 <body>
+<script type="text/javascript">
+	//seta o foco no campo name
+	window.onload = function(){focofield("text");}		
+</script>
 	<h1>Manage Jokes</h1>
 	<p><a href="?add">Add new joke</a></p>
 	<form action="" method="get">
 		<p>View jokes satisfying the following criteria:</p>
-		<dir>
+		<div>
 			<label for="author">By author:</label>
 			<select name="author" id="author">
 				<option value="">Any author</option>
@@ -21,7 +26,25 @@ include INCLUDES . '/helpers.inc.php';
 					<option value="<?php htmlout($author['id']); ?>"> <?php htmlout($author['name']); ?> </option>
 				<?php endforeach; ?>
 			</select>
-		</dir>
+		</div>
+		<div>
+			<label for="category">By category</label>
+			<select name="category" id="category">
+				<option value="">Any category</option>
+				<?php foreach ($categories as $category): ?>
+					<option value="<?php htmlout($category['id']); ?>"><?php htmlout($category['name']); ?></option>
+				<?php endforeach; ?>
+			</select>
+		</div>
+		<div>
+			<label for="text">Containing text:</label>
+			<input type="text" name="text" id="text">
+		</div>
+		<div>
+			<input type="hidden" name="action" value="search">
+			<input type="submit" value="search">
+		</div>
 	</form>
+	<p><a href="..">Return to JMS home</a></p>
 </body>
 </html>
