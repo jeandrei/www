c7b5ee81e020d89ede1bf76f69879eb9b1ae18d5
Jeandrei <jeandreiwalter@gmail.com>
2018-08-24 14:30:52 -0300

    atualizacao

--- desenvolvimento/ninja/chapter7/admin/jokes/index.php
+++ desenvolvimento/ninja/chapter7/admin/jokes/index.php
@@ -1,10 +1,13 @@
 <?php
 include_once('../../includes/constants.inc.php');
 include INCLUDES . '/db.inc.php';
+
+//Build the list of authors
 try 
 {
 	$result = $pdo->query('SELECT id, name FROM author');
-} catch (Exception $e) 
+} 
+catch (Exception $e) 
 {
 	$error = 'Error fetching authors from the database';
 	include 'error.html.php';
@@ -18,10 +21,12 @@ foreach ($result as $row)
 }
 
 
+//Build the list of categories
 try 
 {
 	$result = $pdo->query('SELECT id, name FROM category');
-} catch (Exception $e) 
+} 
+catch (Exception $e) 
 {
 	$error = 'Error fetching categories from database';
 	include 'error.html.php';
@@ -32,7 +37,7 @@ try
 
 foreach ($result as $row) 
 {
-	$categories[] = array('id' => $row['id'], 'name' => $row['name']);
+	$categories[] = array('id' => $row['id'], 'name' => $row['name'], 'selected' => FALSE);
 }
 
 include 'searchform.html.php';
@@ -45,36 +50,40 @@ if(isset($_GET['action']) and $_GET['action'] == 'search')
 	$select = 'SELECT id, joketext';
 	$from = ' FROM joke';
 	$where = ' WHERE TRUE';	
-}
+
 
 $placeholders = array();
 
 if($_GET['author'] != '')
 {
-	$where .= " AND authorid = :authorid";
+	//$where .= " AND authorid = :authorid";
+	$where .= " AND authorid = " . $_GET['authorid'];
 	$placeholders[':authorid'] = $_GET['authorid'];
 }
 
 if($_GET['category'] != '')
 {
 	$from .= ' INNER JOIN jokecategory ON id = jokeid';
-	$where .= " AND categoryid = :categoryid";
+	//$where .= " AND categoryid = :categoryid";
+	$where .= " AND categoryid = " . $_GET['categoryid'];
 	$placeholders[':categoryid'] = $_GET['categoryid'];
 }
 
 if($_GET['text'] =! '')
 {
-	$where .= " AND joketext LIKE :joketext";
+	//$where .= " AND joketext LIKE :joketext";
+	$where .= " AND joketext LIKE %" . $_GET['joketext'] . '%';
 	$placeholders[':joketext'] = '%' . $_GET['joketext'] . '%';
 }
-$sql = $select . $from . $where;
-	echo $sql;
+/*
 try 
 {
 	$sql = $select . $from . $where;
 	$s = $pdo->prepare($sql);
 	$s->execute($placeholders);
-} catch (Exception $e) {
+} 
+catch (Exception $e) 
+{
 	$error = 'Error fetching jokes.';
 	include 'error.html.php';
 	exit();
@@ -83,7 +92,9 @@ try
 foreach ($s as $row) 
 {
 	$jokes[] = array('id' => $row['id'], 'text' => $row['joketext']);
-}
+}*/
+echo $select . $from . $where;
 include 'jokes.html.php';
 exit();
+}
 ?>
