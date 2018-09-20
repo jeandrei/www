59e67fb3f11173bef62ff430a0a03b728f34c659
Jeandrei <jeandreiwalter@gmail.com>
2018-09-20 11:43:02 -0300

    atualização

--- desenvolvimento/oop/the_basic/class_lib.php
+++ desenvolvimento/oop/the_basic/class_lib.php
@@ -16,7 +16,7 @@
 CLASSES = são templates como se fosse um projeto, uma receita usados para gerar objetos, sendo que esses objetos podem executar tarefas ou seja métodos, mais que isso, uma das grandes diferenças entre funções e classes é que a classe tem as duas coisas, variáveis e funções que formam o que chamamos de objeto, quando criamos uma variavel detro de uma classe a chamamos de propriedade.
 	Vantagens evita ter um monte de funções e variaveis flutuando ao redor do código.
 
-OBJETOS = são o resultado gerado pela classe, se a classe fosse uma receita de bolo, o objeto seria o bolo.
+OBJETOS = são o resultado gerado pela classe, se a classe fosse uma receita de bolo, o objeto seria o bolo, é a classe encapsulada.
 
 METODOS = são ações executadas pelo objeto.
 
@@ -27,16 +27,23 @@ METODOS = são ações executadas pelo objeto.
 //Passo 2 Criamos uma classe
 class person {
 	//Passo 3 Adicionamos dados na classe
-	var $name;//Propriedade
+	public $name;//Propriedade
 
 	/*
 	Passo 13 Constructor
 	Constructor permite a inicialização de objetos.
 	Ou seja atribui valores as propriedades quando inicia/cria um objeto.
+	Ao criar uma função construct() o php vai executa-la automaticamente
+	quando criar o objeto.
 	*/
+	function __construct($persons_name) {
+		 $this->name = $persons_name;
+		}
+
+
 
 	//Passo 4 Adicionamos funções a classe
-	function set_name($new_name){	/*
+	public function set_name($new_name){	/*
 									Passo 5	
 									Funções são chamadas de métodos.									
 									Para nomear o método sempre use:
@@ -48,7 +55,7 @@ class person {
 		$this->name = $new_name;
 	}
 
-	function get_name(){
+	public function get_name(){
 		/*
 			Passo 6
 			$this é uma função embutida do php que aponta para o objeto corrente.
@@ -60,5 +67,38 @@ class person {
 }
 
 
+/*
+Passo 15 Restrição de acesso temos:
+Passo 16 restringindo acesso
+public - Não tem restrição de acesso, qualquer um pode acessar a propriedade.
+private - Apenas a mesma classe pode acessar a propriedade.
+protected - Apenas a classe e classes derivadas daquela classe pode acessar a propriedade
+tem a ver com herança.
+*/
+
+/*
+Herança permite a reutilização de código, pense o seguinte.
+Temos uma classe person que representa pessoa e agora queremos criar uma classe employee para representar
+empregado.
+Empregado é uma pessoa, então empregado vai herdar propriedades de pessoa, logo podemos fazer isso com
+oop.
+*/ 
+ class employee extends person {
+ 	function __construct($employee_name)
+ 		/*devido ao fato de employee ser baseado na classe person (herança)
+		a nova classe employee passa a ter acesso a todas os métodos e propriedades
+		da classe person, por isso posso usar o método set_name aqui
+		Obs.: se for necessário que o método execute uma ação diferente do 
+		método da classe pai, pode se criar novamente o método na nova classe
+		ex: protected function set_name($new_name){ ... }
+		Uma vez criado um novo método e for necessário executar o método da classe pai
+		ex: estou dentro da classe employee tenho o método set_name da classe employee, mas
+		quero executar a set_name da classe pai person.
+		Faria assim:
+		person::set_name($new_name); ou
+		parent::set_name($new_name);
+ 		*/
+ 		$this->set_name($employee_name);
+ }
 
 ?>
--- desenvolvimento/oop/the_basic/index.php
+++ desenvolvimento/oop/the_basic/index.php
@@ -10,18 +10,20 @@
 <?php
 
 //Passo 8 Instanciamos/Criamos nosso objeto.
-$stefan = new person();
+//$stefan = new person();
 
 //Passo 9 a palavra chave new
 //podemos criar vários objetos da mesma classe
 //no ponte de vista do php cada objeto é sua própria entidade
 //os dois são da mesma classe person mas são objetos totalmente diferentes
-$jimmy = new person();
+//$jimmy = new person();
+$stefan = new person("Stefan Mischook");
+$jimmy = new person("Nick Waddles");
 
 /*Passo 10 Alteramos as propriedades dos objetos
 utilizando os métodos*/
-$stefan->set_name("Stefan Mischook");
-$jimmy->set_name("Nick Waddles");
+//$stefan->set_name("Stefan Mischook");
+//$jimmy->set_name("Nick Waddles");
 
 /*
 Passo 11 Acessando os dados de um objeto
@@ -30,7 +32,7 @@ echo "Stefan's full name: " . $stefan->get_name();
 echo "<br>Nick's full name: " . $jimmy->get_name();
 
 /*
-Passo 12 Nunca acesse uma propriedade diretamente
+Passo 12 Nunca acesse uma propriedade diretamente, pode ser feito, mas não é uma boa prática
 Como no exemplo acima echo "Stefan's full name: " . $stefan->name;
 Ao invés crie um método para recuperar o valor desta propriedade neste caso get_name
 echo "Stefan's full name: " . $stefan->get_name();
