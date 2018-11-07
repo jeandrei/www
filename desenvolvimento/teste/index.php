90a9c52b11c766e1dabfb5a4d62246fc1b166f6b
Jeandrei <jeandreiwalter@gmail.com>
2018-11-07 17:14:23 -0200

    atualizacao

---
+++ desenvolvimento/teste/index.php
@@ -0,0 +1,6 @@
+<?php
+var_dump($_SERVER);
+echo "***********************************************************************************<br>";
+var_dump($_ENV);
+echo "<br>";
+echo $_SERVER['REQUEST_URI'];
