db221a6af04beda0e6a8229e1154da08566f9a67
Jeandrei <jeandreiwalter@gmail.com>
2018-11-09 10:13:09 -0200

    bkpmvc

---
+++ bkpdev/mvc/app/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+// Load Config
+require_once 'config/config.php';
+
+// Load Libraries
+// apenas carrega todos os arquivos necessários
+// para não ter que adicionar uma lista de arquivos em todas as páginas
+// carregamos apenas o bootstrap.php
+
+
+// Autoload Core Libraries vai ler automaticamente 
+//dar um require_once em todos os arquivos dentro de libraries
+spl_autoload_register(
+    function($className){
+    require_once 'libraries/' . $className . '.php';
+    }
+);//spl_autoload_register
--- bkpdev/mvc/app/libraries/Core.php
+++ bkpdev/mvc/app/libraries/Core.php
@@ -9,7 +9,7 @@
 class Core{
     //se for para o endereço localhost/mvc vai automáticamente para o controlador Pages
     protected $currentController = 'Pages';
-    // se não for passado nenhum metodo como localhost/mvc/post ele vai direto para o views/pages/index.php
+    // se não for passado nenhum metodo como localhost/mvc/post ele vai direto para o public/index.php
     protected $currentMethod = 'index';
     // se não passar nenhum parâmetro ex localhost/mvc/post/edit/1 que aqui 1 é o parâmetro
     // montamos um array vazio
@@ -39,16 +39,12 @@ class Core{
             // passamos o controller para a propriedade currentController vai substituir
             // Pages que é o valor padrão pelo valor do indice 0 da variável url neste caso Pages
             $this->currentController = ucwords($url[0]);            
-            // Unset 0 Indexs             
+            // Unset 0 Indexs                              
             unset($url[0]);
             // fica no array para a o passo 5 Array [1]=> string(5) "about" [2]=> string(2) "33"
         }
 
         // Require the controller
-        // Aqui ele vai requerer o arquivo com base nas propriedades da classe Core
-        // Se na propriedade currentController não for passado nada o valor padrão é Pages
-        // Logo vai montar
-        // (referência 1) require once '../app/controllers/Pages.php'; 
         require_once '../app/controllers/' . $this->currentController . '.php';       
         //Instantiate controller class
         // exemplo que está montando na linha abaixo $pages = new Pages;
@@ -94,13 +90,7 @@ class Core{
         // caso contrário recebe um array vazio
         // que é o valor padrão da propriedade params
         $this->params = $url ? array_values($url) : []; 
-        /* Call a callback with array of params
-        
-        se não passar nada na url com valores padrão
-        call_user_func_array([Pages, index], []]);
-        está executando a classe Pages método index do arquivo /app/controllers/Pages.php
-        que foi requerido/adicionado onde eu coloquei referência 1
-        */
+        // Call a callback with array of params
         call_user_func_array([$this->currentController, 
         $this->currentMethod], $this->params);
         //**********************FIM CAPTURAR O PARÂMETRO DA URL**************************
@@ -117,8 +107,6 @@ class Core{
     isso foi configurado em nosso arquivo public/.htasses
     se a url for loclhost/mvc/pages/about/33
     o resultado do $_GET['url'] seria pages/about/33
-    só entra na função getUrl se for passado alguma coisa depois do /mvc
-    caso contrário ele carrega o /views/pages/index
     */
     public function getUrl(){
         //testamos se existe alguma coisa no GET
@@ -132,7 +120,7 @@ class Core{
             // retornamos o array com os dados da url
             // ao final se tivermos a url /mvc/pages/about/33
             // o método vai retornar o seguinte array
-            // Array ( [0] => pages [1] => about [2] => 33 )            
+            // Array ( [0] => pages [1] => about [2] => 33 )
             return $url;
         }
     }
