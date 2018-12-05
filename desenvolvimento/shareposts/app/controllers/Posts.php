5003d47ac555c0fdf9956e142b4c7542105e7775
Jeandrei <jeandreiwalter@gmail.com>
2018-12-05 17:50:49 -0200

    delete

--- desenvolvimento/shareposts/app/controllers/Posts.php
+++ desenvolvimento/shareposts/app/controllers/Posts.php
@@ -134,4 +134,25 @@
          ];
          $this->view('posts/show', $data);
      }
+
+     public function delete($id){         
+         if($_SERVER['REQUEST_METHOD'] == 'POST'){
+              // Get existing pst for model
+            $post = $this->postModel->getPostById($id);
+
+            //Check for owner
+            if($post->user_id != $_SESSION['user_id']){
+                redirect('posts');
+            }             
+            
+            if($this->postModel->deletePost($id)){
+                flash('post_message', 'Post Removed');
+                redirect('posts');
+            } else {
+                die('Someting went wrong');
+            }
+         } else {
+             redirect('posts');
+         }         
+     }
  }//class
