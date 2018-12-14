5ce76072c4a7befbf4e84f7a22b5879c23520e69
Jeandrei <jeandreiwalter@gmail.com>
2018-12-14 16:34:35 -0200

    melhoria formulario

--- desenvolvimento/sisurpe/app/views/datausers/index.php
+++ desenvolvimento/sisurpe/app/views/datausers/index.php
@@ -15,66 +15,74 @@
             <input class="form-control form-control-sm" type="text" placeholder="<?php echo $data['nome_aluno']; ?>" readonly>         
         </div>
 
-
-
-<!--NASCIMENTO E SEXO-->
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="nascimento">Nascimento</label>
-                <input class="form-control" name="nascimento" type="date" value="">
-            </div>
-            <div class="form-group col-md-6">
-            <label for="nascimento">Sexo</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck1" checked>
-                    <label class="form-check-label" for="defaultCheck1">
-                        Masculino
-                    </label>
-                    </div>
-                    <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
-                    <label class="form-check-label" for="defaultCheck2">
-                        Feminino
-                    </label>
-                </div>
-            </div>
+<!--NASCIMENTO NACIONALIDADE E NATURALIDADE-->
+<div class="form-row">
+    <div class="form-group col-md-4">
+    <label for="nascimento">Nascimento</label>
+    <input class="form-control" name="nascimento" type="date" value="">
     </div>
-
-<!--TELEFONE E EMAIL-->  
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="telefone">Telefone</label>
-      <input type="tel" name="telefone" id="telefone" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="Telefone do aluno">      
+    <div class="form-group col-md-4">
+    <label for="nacionalidade">Nacionalidade</label>
+      <input type="text" name="nacionalidade" id="nacionalidade" class="form-control" placeholder="Nacionalidade do aluno">         
     </div>
-    <div class="form-group col-md-6">
-      <label for="email_aluno">Email</label>
-      <input type="email" class="form-control" id="email_aluno" placeholder="Email do aluno">
+    <div class="form-group col-md-4">
+    <label for="naturalidade">Naturalidade</label>
+      <input type="text" class="form-control" id="naturalidade" placeholder="Naturalidade do aluno">
     </div>
   </div>
+
+  <!--TELEFONE EMAIL E SEXO-->
+<div class="form-row">
+        <div class="form-group col-md-4">
+        <label for="telefone">Telefone</label>
+          <input type="tel" name="telefone" id="telefone" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="(99) 99999-9999">
+        </div>
+        <div class="form-group col-md-4">
+        <label for="email_aluno">Email</label>
+          <input type="email" class="form-control" id="email_aluno" placeholder="Email do aluno">        
+        </div>
+        <div class="form-group col-md-4">  <legend class="col-form-label col-sm-2 pt-0">Sexo</legend>
+            <div class="col-sm-10">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="sexo" id="sexom" value="M" checked>
+                <label class="form-check-label" for="sexom">
+                  Masculino
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="sexo" id="sexof" value="option2">
+                <label class="form-check-label" for="sexof">
+                  Feminino
+                </label>
+              </div>        
+            </div>
+        </div>
+  </div>
+
 
 
 <!--NOME PAI E TELEFONE-->  
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-8">
       <label for="nome_pai">Nome do pai</label>
       <input type="text" class="form-control" id="nome_pai" placeholder="Nome do pai">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-4">
       <label for="telefone_pai">Telefone do pai</label>
-      <input type="tel" name="telefone_pai" id="telefone_pai" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="Telefone do aluno">    
+      <input type="tel" name="telefone_pai" id="telefone_pai" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="(99) 99999-9999">    
     </div>
   </div>
 
 
   <!--NOME MÃE E TELEFONE-->  
   <div class="form-row">
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-8">
       <label for="nome_mae">Nome da mãe</label>
       <input type="text" class="form-control" id="nome_mae" placeholder="Nome da mãe">
     </div>
-    <div class="form-group col-md-6">
+    <div class="form-group col-md-4">
       <label for="telefone_mae">Telefone da mãe</label>
-      <input type="tel" name="telefone_mae" id="telefone_mae" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="Telefone do aluno">    
+      <input type="tel" name="telefone_mae" id="telefone_mae" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="(99) 99999-9999">  
     </div>
   </div>
 
@@ -87,7 +95,7 @@
     </div>
     <div class="form-group col-md-6">
       <label for="telefone_responsavel">Telefone do reponsavel</label>
-      <input type="tel" name="telefone_responsavel" id="telefone_responsavel" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="Telefone do aluno">    
+      <input type="tel" name="telefone_responsavel" id="telefone_responsavel" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="(99) 99999-9999">    
     </div>
   </div>
 
@@ -96,11 +104,11 @@
  <div class="form-row">
     <div class="form-group col-md-6">
       <label for="naturalidade">Naturalidade</label>
-      <input type="text" class="form-control" id="naturalidade" placeholder="Aluno naturalidade">
+      <input type="text" class="form-control" id="naturalidade" placeholder="Naturalidade do aluno">
     </div>
     <div class="form-group col-md-6">
       <label for="nacionalidade">Nacionalidade</label>
-      <input type="text" name="nacionalidade" id="nacionalidade" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="Aluno nacionalidade">    
+      <input type="text" name="nacionalidade" id="nacionalidade" class="form-control" placeholder="Nacionalidade do aluno">    
     </div>
   </div>
 
