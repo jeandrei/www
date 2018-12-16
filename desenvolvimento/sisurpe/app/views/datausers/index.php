58044aba46e9d29b3cd68bbe57afbe688202a785
Jeandrei <jeandreiwalter@gmail.com>
2018-12-16 18:59:08 -0200

    reorganizacao

--- desenvolvimento/sisurpe/app/views/datausers/index.php
+++ desenvolvimento/sisurpe/app/views/datausers/index.php
@@ -18,29 +18,57 @@
 <!--NASCIMENTO NACIONALIDADE E NATURALIDADE TELEFONE-->
 <div class="form-row">
     <div class="form-group col-md-2">
-    <label for="nascimento">Nascimento</label>
-    <input class="form-control" name="nascimento" type="date" value="">
+        <label for="nascimento">Nascimento</label>
+        <input 
+          class="form-control"
+          type="date"  
+          name="nascimento"
+          id="nascimento"         
+          value="">
     </div>
     <div class="form-group col-md-4">
-    <label for="nacionalidade">Nacionalidade</label>
-      <input type="text" name="nacionalidade" id="nacionalidade" class="form-control" placeholder="Nacionalidade do aluno">         
+        <label for="nacionalidade">Nacionalidade</label>
+        <input 
+          class="form-control" 
+          type="text" 
+          name="nacionalidade" 
+          id="nacionalidade"          
+          placeholder="Nacionalidade do aluno">         
     </div>
     <div class="form-group col-md-4">
-    <label for="naturalidade">Naturalidade</label>
-      <input type="text" class="form-control" id="naturalidade" placeholder="Naturalidade do aluno">
+        <label for="naturalidade">Naturalidade</label>
+        <input 
+          class="form-control" 
+          type="text"
+          name="naturalidade" 
+          id="naturalidade"                 
+          placeholder="Naturalidade do aluno">
     </div>    
   </div>
 
+
   <!--TELEFONE EMAIL E SEXO-->
 <div class="form-row">
-        <div class="form-group col-md-2">
+    <div class="form-group col-md-2">
         <label for="telefone">Telefone</label>
-          <input type="tel" name="telefone" id="telefone" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="(99) 99999-9999">
-        </div>
-        <div class="form-group col-md-6">
+        <input 
+          class="form-control" 
+          type="tel" 
+          name="telefone" 
+          id="telefone"          
+          maxlength="15"          
+          placeholder="(99) 99999-9999"
+          onkeypress="mascara( this, mtel );">
+    </div>
+    <div class="form-group col-md-6">
         <label for="email_aluno">Email</label>
-          <input type="email" class="form-control" id="email_aluno" placeholder="Email do aluno">        
-        </div>
+        <input 
+          class="form-control" 
+          type="email"            
+          name="email_aluno" 
+          id="email_aluno" 
+          placeholder="Email do aluno">        
+    </div>
         <div class="form-group col-md-4">  <legend class="col-form-label col-sm-2 pt-0">Sexo</legend>
             <div class="col-sm-10">
               <div class="form-check">
@@ -62,23 +90,35 @@
 
 
 <!--NOME PAI E TELEFONE-->  
-  <div class="form-row">
+<div class="form-row">
     <div class="form-group col-md-8">
-      <label for="nome_pai">Nome do pai</label>
-      <input type="text" class="form-control" id="nome_pai" placeholder="Nome do pai">
+        <label for="nome_pai">Nome do pai</label>
+        <input 
+          class="form-control"
+          type="text"           
+          name="nome_pai" 
+          id="nome_pai" 
+          placeholder="Nome do pai">
     </div>
     <div class="form-group col-md-2">
-      <label for="telefone_pai">Telefone do pai</label>
-      <input type="tel" name="telefone_pai" id="telefone_pai" onkeypress="mascara( this, mtel );" maxlength="15" class="form-control" placeholder="(99) 99999-9999">    
+        <label for="telefone_pai">Telefone do pai</label>
+        <input 
+          class="form-control" 
+          type="tel" 
+          name="telefone_pai" 
+          id="telefone_pai"          
+          maxlength="15"           
+          placeholder="(99) 99999-9999"
+          onkeypress="mascara( this, mtel );" >    
     </div>
-  </div>
+</div>
 
 
   <!--NOME MÃE E TELEFONE-->  
   <div class="form-row">
     <div class="form-group col-md-8">
       <label for="nome_mae">Nome da mãe</label>
-      <input type="text" class="form-control" id="nome_mae" placeholder="Nome da mãe">
+      <input type="text" class="form-control" name="nome_mae" id="nome_mae" placeholder="Nome da mãe">
     </div>
     <div class="form-group col-md-2">
       <label for="telefone_mae">Telefone da mãe</label>
@@ -91,7 +131,7 @@
   <div class="form-row">
     <div class="form-group col-md-8">
       <label for="nome_responsavel">Nome do responsavel</label>
-      <input type="text" class="form-control" id="nome_responsavel" placeholder="Nome do responsavel">
+      <input type="text" class="form-control" name="nome_responsavel" id="nome_responsavel" placeholder="Nome do responsavel">
     </div>
     <div class="form-group col-md-2">
       <label for="telefone_responsavel">Telefone do reponsavel</label>
@@ -104,29 +144,70 @@
  <div class="form-row">
     <div class="form-group col-md-2">
       <label for="rg">RG</label>
-      <input type="text" class="form-control" id="rg" placeholder="RG do aluno">
+      <input type="text" class="form-control" name="rg" id="rg" placeholder="RG do aluno">
     </div>
     <div class="form-group col-md-1">
-      <label for="ufrg">UF RG</label>
-      <input type="text" name="ufrg" id="ufrg" class="form-control" placeholder="UF RG">    
+      <label for="ufrg">UF</label>
+      <input type="text" class="form-control" name="ufrg" id="ufrg"  placeholder="UF RG">    
     </div>
     <div class="form-group col-md-2">
       <label for="ufrg">Órgão Emissor</label>
-      <input type="text" name="rg_oremis" id="rg_oremis" class="form-control" placeholder="Órgão emissor do RG">    
+      <input type="text" class="form-control" name="rg_oremis" id="rg_oremis" placeholder="Órgão emissor do RG">    
     </div>
     <div class="form-group col-md-2">
       <label for="ufrg">Título de eleitor</label>
-      <input type="text" name="titulo" id="titulo" class="form-control" placeholder="Número do título de eleitor">    
+      <input type="text" class="form-control" name="titulo" id="titulo" placeholder="Número do título de eleitor">    
     </div>
     <div class="form-group col-md-2">
       <label for="ufrg">Zona</label>
-      <input type="text" name="zona" id="zona" class="form-control" placeholder="Zona eleitoral">    
+      <input type="text" class="form-control" name="zona" id="zona" placeholder="Zona eleitoral">    
     </div>
     <div class="form-group col-md-2">
       <label for="ufrg">Seção</label>
-      <input type="text" name="secao" id="secao" class="form-control" placeholder="Seção eleitoral">    
+      <input type="text" class="form-control" name="secao" id="secao" placeholder="Seção eleitoral">    
     </div>
   </div>
+
+
+
+ <!--DADOS DA CERTIDÃO-->  
+ <div class="form-row">
+    <div class="form-group col-md-2">
+      <label for="rg">Certidão de nascimento</label>
+      <input type="text" class="form-control" name="num_cert" id="num_cert" placeholder="Número da certidão">
+    </div>
+    <div class="form-group col-md-1">
+      <label for="ufrg">UF</label>
+      <input type="text" name="uf_cert" id="uf_cert" class="form-control" placeholder="UF">    
+    </div>
+    <div class="form-group col-md-2">
+      <label for="ufrg">Cartório Certidão</label>
+      <input type="text" name="cart_cert" id="cart_cert" class="form-control" placeholder="Cartório certidão">    
+    </div>
+    <div class="form-group col-md-2">
+      <label for="ufrg">Modelo</label>
+      <input type="text" name="modelo" id="modelo" class="form-control" placeholder="Modelo">    
+    </div>    
+    <div class="form-group col-md-2">
+      <label for="ufrg">Folha</label>
+      <input type="text" name="folha" id="folha" class="form-control" placeholder="Folha">    
+    </div>
+    <div class="form-group col-md-2">
+      <label for="ufrg">Data emissão</label>
+      <input type="text" name="data_em" id="data_em" class="form-control" placeholder="Data emissão">    
+    </div>
+    <div class="form-group col-md-2">
+      <label for="ufrg">Município da certidão</label>
+      <input type="text" name="mun_cert" id="mun_cert" class="form-control" placeholder="Município certidão">    
+    </div>
+  </div>
+
+
+
+
+
+
+
 
 
   <div class="form-group">
--- desenvolvimento/sisurpe/app/views/inc/header.php
+++ desenvolvimento/sisurpe/app/views/inc/header.php
@@ -5,20 +5,22 @@
 <head> 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!--Linha abaixo é o link para o bootstrap direto da internet-->    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <!--Link abaixo é do Font Awesome cdn-->
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> 
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/bootstrap.min.css">
+    <!--CSS MIDIFICAÇÕES SOBESCREVER Botstrap-->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <!--Botstrap main-->
+    <script src="<?php echo URLROOT; ?>/public/js/bootstrap.min.js"></script>
+    <!--Javascript funções-->
     <script src="<?php echo URLROOT; ?>/public/js/main.js"></script>
+     <!--jquery-->
     <script src="<?php echo URLROOT; ?>/public/js/jquery-3.3.1.min.js"></script> 
+    <!--jquery mask-->
     <script src="<?php echo URLROOT; ?>/public/js/jquery.mask.js" data-autoinit="true"></script> 
     <title><?php echo SITENAME; ?></title>
 </head>
 <body>
 <?php require APPROOT . '/views/inc/navbar.php'; ?>
 <!-- a linha abaixo inicia um container do bootstrap ela vai fechr no arquivo footer.php-->
-<div class="container">
-    
-<?php //echo utf8_encode('Olá pessoal.');?>
+<div class="container">   
