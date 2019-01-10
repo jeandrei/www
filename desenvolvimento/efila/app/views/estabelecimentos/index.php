1c914ba2b1f98cd4699c32395d9821236e98d3f2
Jeandrei <jeandreiwalter@gmail.com>
2019-01-10 15:26:56 -0200

    tabela estabelecimentos

--- desenvolvimento/efila/app/views/estabelecimentos/index.php
+++ desenvolvimento/efila/app/views/estabelecimentos/index.php
@@ -1,36 +1,49 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
+
+
 <?php flash('post_message');?>
- <div class="row align-items-center mb-3">
-    <div class="col-md-6">
-        <h1>Estabelecimentos de Ensino</h1>
-    </div>
-    <div class="col-md-6">
-        <a href="<?php echo URLROOT; ?>/estabelecimentos/add" class="btn btn-primary float-right">
-            <i class="fa fa-pencil"></i> Adicionar
-        </a>
-    </div>
- </div> 
 
+<hr>
 
-
- <!--TABELA-->
- <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Nome</th>
-            </tr>
-        </thead>
+<!-- TABELA -->
+<div class="card">
+  <h3 class="card-header text-center font-weight-bold text-uppercase py-4">ESTABELECIMENTOS</h3>
+  <div class="card-body">
+    <div id="table" class="">
+      
+      <table class="table table-bordered table-responsive-md table-striped text-center">
+        <tr>
+          <th class="text-left col-md-6">NOME DO ESTABELECIMENTO</th>          
+          <th class="text-center"></th>
+          <th class="text-center"></th>
+        </tr>
         
-        <?php foreach($data['estabelecimentos'] as $registro) : ?>    
-        
-        <tbody>
-            <tr>
-                <td><?php echo $registro->nome;?></td>
-            </tr>
-        </tbody>    
-        </div>
-    <?php endforeach; ?>
-</table>
+        <?php foreach($data['estabelecimentos'] as $registro) : ?> 
 
+        <tr>
+        <td class="pt-3-half text-left"><?php echo $registro->nome;?></td>
+         
+          <td>
+            <span class="table-edit"><a href="<?php echo URLROOT; ?>/estabelecimentos/edit/<?php echo $registro->id; ?>" class="btn btn-primary btn-rounded btn-sm my-0">Editar</a></span>            
+          </td>
+          <td>
+            <span class="table-remove"><a href="<?php echo URLROOT; ?>/estabelecimentos/delete/<?php echo $registro->id; ?>" class="btn btn-danger btn-rounded btn-sm my-0">Remover</a></span>            
+          </td>
+        </tr>
+        <?php endforeach; ?>  
+      </table>
+    </div>
+  </div>
+</div>
+<!-- Editable table -->
+
+<hr>
+
+<!-- Adicionar -->
+<div class="col-md-6">
+    <a href="<?php echo URLROOT; ?>/estabelecimentos/add" class="btn btn-success float-left">
+        <i class="fa fa-pencil"></i> Adicionar
+    </a>
+</div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
--- dumps/efila.sql
+++ dumps/efila.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 09/01/2019 às 13:29
--- Versão do servidor: 5.7.23
--- Versão do PHP: 7.2.8
+-- Generation Time: 10-Jan-2019 às 15:09
+-- Versão do servidor: 5.7.24
+-- versão do PHP: 7.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `efila`
+-- Database: `efila`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `aluno`
+-- Estrutura da tabela `aluno`
 --
 
 CREATE TABLE `aluno` (
@@ -38,7 +38,7 @@ CREATE TABLE `aluno` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `atendimento`
+-- Estrutura da tabela `atendimento`
 --
 
 CREATE TABLE `atendimento` (
@@ -52,7 +52,7 @@ CREATE TABLE `atendimento` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `estabelecimento`
+-- Estrutura da tabela `estabelecimento`
 --
 
 CREATE TABLE `estabelecimento` (
@@ -61,10 +61,19 @@ CREATE TABLE `estabelecimento` (
   `endereco` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `estabelecimento`
+--
+
+INSERT INTO `estabelecimento` (`id`, `nome`, `endereco`) VALUES
+(1, 'Escola teste', 'rua da silva 35'),
+(2, 'escola teste 2', 'rua josé carlos 47'),
+(3, 'adfasf', 'adfssdf');
+
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `fila_estabelecimento`
+-- Estrutura da tabela `fila_estabelecimento`
 --
 
 CREATE TABLE `fila_estabelecimento` (
@@ -79,7 +88,7 @@ CREATE TABLE `fila_estabelecimento` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `posts`
+-- Estrutura da tabela `posts`
 --
 
 CREATE TABLE `posts` (
@@ -91,7 +100,7 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `posts`
+-- Extraindo dados da tabela `posts`
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `created_at`) VALUES
@@ -101,7 +110,7 @@ INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
@@ -110,49 +119,51 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `telefone1` varchar(20) NOT NULL,
-  `desctel1` varchar(100) NOT NULL,
+  `telefone1` varchar(20) DEFAULT NULL,
+  `desctel1` varchar(100) DEFAULT NULL,
   `telefone2` varchar(20) DEFAULT NULL,
   `desctel2` varchar(100) DEFAULT NULL,
-  `telefone3` varchar(20) DEFAULT NULL,
-  `dsctel3` varchar(100) DEFAULT NULL
+  `type` char(5) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Fazendo dump de dados para tabela `users`
+-- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `telefone1`, `desctel1`, `telefone2`, `desctel2`, `telefone3`, `dsctel3`) VALUES
-(1, 'Jeandrei', 'jeandreiwalter@gmail.com', '$2y$10$lyyCqzV/cJw5A8TpddC47Ow8K2iVHOHbKl.Nzs0fm/CgjuDBRZoMq', '2018-11-23 10:19:18', '', '', '', '', '', ''),
-(2, 'teste1', 'teste1r@gmail.com', '$2y$10$Y3Phy8lW7ACZ41qrXjqOjuS26Jzj5WEoWa3mjNrNwWcHpyPKnOtji', '2018-11-27 15:29:36', '', '', '', '', '', ''),
-(3, 'Dexter', 'dexter@gmail.com', '$2y$10$xS7RXsPMbTaUx9JVG8fqMO3aFrqQPUqjcLhLxmMmfS9gyLD/SqrIy', '2019-01-09 12:34:26', '', '', '', '', '', '');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `telefone1`, `desctel1`, `telefone2`, `desctel2`, `type`) VALUES
+(1, 'Jeandrei', 'jeandreiwalter@gmail.com', '$2y$10$lyyCqzV/cJw5A8TpddC47Ow8K2iVHOHbKl.Nzs0fm/CgjuDBRZoMq', '2018-11-23 10:19:18', '', '', '', '', 'admin'),
+(2, 'teste1', 'teste1r@gmail.com', '$2y$10$Y3Phy8lW7ACZ41qrXjqOjuS26Jzj5WEoWa3mjNrNwWcHpyPKnOtji', '2018-11-27 15:29:36', '', '', '', '', 'user'),
+(3, 'Dexter', 'dexter@gmail.com', '$2y$10$xS7RXsPMbTaUx9JVG8fqMO3aFrqQPUqjcLhLxmMmfS9gyLD/SqrIy', '2019-01-09 12:34:26', '', '', '', '', 'user'),
+(4, 'jean', 'jeandrei.walter@penha.sc.gov.br', '$2y$10$//zWWho7kVCC7jMXFPZeMuFTAyQgL4Awjy9S0m.1S0PMa/qLBQOru', '2019-01-09 16:49:58', NULL, NULL, NULL, NULL, 'user'),
+(6, 'biju', 'biju@gmail.com', '$2y$10$ddCIFSoS7VKaJCAMFqlenenZLAfFw38AqDJXRKOaWcL0qqLW881Y6', '2019-01-09 17:04:05', '(47) 99587-2547', 'casa', '(47) 99587-9874', 'trabalho', 'user'),
+(7, 'Arlete', 'arlete@gmail.com', '$2y$10$nUAZY2gpZvobmJDNEKJHr.cT4B1xpB89xDgu94hxyTjGmhp4/O5lG', '2019-01-09 17:35:50', '(55) 12315-4545', 'casa', '(54) 54654-5456', 'trabalho', 'user');
 
 --
--- Índices de tabelas apagadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `aluno`
+-- Indexes for table `aluno`
 --
 ALTER TABLE `aluno`
   ADD PRIMARY KEY (`id`),
   ADD KEY `usuario_id` (`usuario_id`);
 
 --
--- Índices de tabela `atendimento`
+-- Indexes for table `atendimento`
 --
 ALTER TABLE `atendimento`
   ADD PRIMARY KEY (`id`),
   ADD KEY `estebelecimento_id` (`estebelecimento_id`);
 
 --
--- Índices de tabela `estabelecimento`
+-- Indexes for table `estabelecimento`
 --
 ALTER TABLE `estabelecimento`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `fila_estabelecimento`
+-- Indexes for table `fila_estabelecimento`
 --
 ALTER TABLE `fila_estabelecimento`
   ADD PRIMARY KEY (`id`),
@@ -161,75 +172,75 @@ ALTER TABLE `fila_estabelecimento`
   ADD KEY `aluno_id` (`aluno_id`) USING BTREE;
 
 --
--- Índices de tabela `posts`
+-- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `aluno`
+-- AUTO_INCREMENT for table `aluno`
 --
 ALTER TABLE `aluno`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `atendimento`
+-- AUTO_INCREMENT for table `atendimento`
 --
 ALTER TABLE `atendimento`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `estabelecimento`
+-- AUTO_INCREMENT for table `estabelecimento`
 --
 ALTER TABLE `estabelecimento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de tabela `fila_estabelecimento`
+-- AUTO_INCREMENT for table `fila_estabelecimento`
 --
 ALTER TABLE `fila_estabelecimento`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `posts`
+-- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de tabela `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Restrições para dumps de tabelas
+-- Constraints for dumped tables
 --
 
 --
--- Restrições para tabelas `aluno`
+-- Limitadores para a tabela `aluno`
 --
 ALTER TABLE `aluno`
   ADD CONSTRAINT `aluno_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`);
 
 --
--- Restrições para tabelas `atendimento`
+-- Limitadores para a tabela `atendimento`
 --
 ALTER TABLE `atendimento`
   ADD CONSTRAINT `atendimento_ibfk_1` FOREIGN KEY (`estebelecimento_id`) REFERENCES `estabelecimento` (`id`);
 
 --
--- Restrições para tabelas `fila_estabelecimento`
+-- Limitadores para a tabela `fila_estabelecimento`
 --
 ALTER TABLE `fila_estabelecimento`
   ADD CONSTRAINT `fila_estabelecimento_ibfk_1` FOREIGN KEY (`aluno_id`) REFERENCES `aluno` (`id`),
