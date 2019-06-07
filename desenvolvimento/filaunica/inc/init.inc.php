e673f05deb1be90eca2543f00ca9c7de8628a373
Jeandrei <jeandreiwalter@gmail.com>
2019-06-07 09:37:16 -0300

    remocao filaunica

--- desenvolvimento/filaunica/inc/init.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once ($_SERVER["DOCUMENT_ROOT"] . '/filaunica/inc/config.inc.php');
-require_once ($_SERVER["DOCUMENT_ROOT"] . '/filaunica/inc/db.inc.php');
-require_once ($_SERVER["DOCUMENT_ROOT"] . '/filaunica/inc/helpers.inc.php');
-require_once ($_SERVER["DOCUMENT_ROOT"] . '/filaunica/inc/magicquotes.inc.php');
-?>
