2b091f90ae38ebd0cf27c548e0103213f6ff962a
Jeandrei <jeandreiwalter@gmail.com>
2019-06-10 18:24:39 -0300

    inicio classe Database

--- desenvolvimento/mvc/app/models/Post.php
+++ desenvolvimento/mvc/app/models/Post.php
@@ -1,12 +1,9 @@
 <?php
-    // Reconhece a classe Database pois no arquivo bootstrap.php tem o autoload que lê
-    // o que está dentro de libraries em que libraries tem Database.php
-    // model interage com o banco e passa os dados para o controller, o controller seleciona o view
-    // e passa os dados obtidos pelo model
-    class Post {
+    class Post{
         private $db;
 
         public function __construct(){
             $this->db = new Database;
         }
+
     }
