414fcda8404377910552bfb99d620c8de19918ea
Jeandrei <jeandreiwalter@gmail.com>
2019-06-09 21:53:49 -0300

    inicio classe core

--- desenvolvimento/mvc/app/libraries/Core.php
+++ desenvolvimento/mvc/app/libraries/Core.php
@@ -11,10 +11,38 @@
      protected $params = [];
 
      public function __construct(){
-         $this->getUrl();
+         //print_r($this->getUrl());
+         $url = $this->getUrl();
+
+         // Look in controller for first value
+         // verificamos se o controller existe dentro da pasta app/controllers
+         if(file_exists('../app/controllers/' . ucwords($url[0]). '.php')){
+            //if existe, set as a controller
+            $this->currentController = ucwords($url[0]);
+            // Unset 0 index
+            unset($url[0]);
+         }
+         
+         // Require the controller
+         require_once '../app/controllers/' . $this->currentController . '.php';
+
+         // Instantiate controller class
+         // o que vai acontecer aqui é instanciar o controller
+         // exemplo $pages = new Pages;
+         $this->currentController = new $this->currentController;
+
+
      }
 
      public function getUrl(){
-         echo $_GET['url'];
+        if(isset($_GET['url'])){
+          $url = rtrim($_GET['url'], '/');
+          //valida a url FILTER_SANITIZE_URL não deixa passar
+          // nenhum caractere que uma url não deve ter 
+          $url = filter_var($url, FILTER_SANITIZE_URL);
+          //quebramos a url em um array
+          $url = explode('/', $url);
+          return $url;
+        }
      }
  }
--- desenvolvimento/mvc/public/index.php
+++ desenvolvimento/mvc/public/index.php
@@ -1,4 +1,7 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', true);
+    
     require_once '../app/bootstrap.php';
 
     // Init Core Library
