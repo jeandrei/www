5e4a52b0fde4a6ab2a518c6eff944e307e0c499c
Jeandrei <jeandreiwalter@gmail.com>
2019-06-17 22:35:57 -0300

    formulário registro e login

--- desenvolvimento/shareposts/app/controllers/Users.php
+++ desenvolvimento/shareposts/app/controllers/Users.php
@@ -1,13 +1,72 @@
 <?php 
     class Users extends Controller{
         public function __construct(){
-
+           
         }
 
         public function register(){
             // Check for POST            
             if($_SERVER['REQUEST_METHOD'] == 'POST'){
                 // Process form
+
+                // Sanitize POST data
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                //init data
+                $data = [
+                    'name' => trim($_POST['name']),
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'confirm_password' => trim($_POST['confirm_password']),
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => ''
+                ];                
+
+                
+
+                // Validate Email
+                if(empty($data['email'])){
+                    $data['email_err'] = 'Please enter email';
+                }
+
+                // Validate Name
+                if(empty($data['name'])){
+                    $data['name_err'] = 'Please enter name';
+                }
+
+                 // Validate Password
+                 if(empty($data['password'])){
+                    $data['password_err'] = 'Please enter password';
+                } elseif (strlen($data['password']) < 6){
+                    $data['password_err'] = 'Password must be at least 6 characters';
+                }
+
+                // Validate Confirm Password
+                if(empty($data['confirm_password'])){
+                    $data['confirm_password_err'] = 'Please confirm password';
+                } else {
+                    if($data['password'] != $data['confirm_password']){
+                    $data['confirm_password_err'] = 'Passwords do not match';    
+                    }
+                }
+
+                // Make sure errors are empty
+                if(                    
+                    empty($data['email_err']) &&
+                    empty($data['name_err']) && 
+                    empty($data['password_err']) &&
+                    empty($data['confirm_password_err']) 
+                    ){
+                      //Validate
+                      die('SUCCESS');
+                    } else {
+                      // Load the view with errors
+                      $this->view('users/register', $data);
+                    }               
+
+            
             } else {
                 // Init data
                 $data = [
@@ -22,6 +81,23 @@
                 ];
                 // Load view
                 $this->view('users/register', $data);
+            }
+        }
+
+        public function login(){
+            // Check for POST            
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                // Process form
+            } else {
+                // Init data
+                $data = [                    
+                    'email' => '',
+                    'password' => '',  
+                    'email_err' => '',
+                    'password_err' => ''                    
+                ];
+                // Load view
+                $this->view('users/login', $data);
             }
         }
     }
--- desenvolvimento/shareposts/app/views/users/register.php
+++ desenvolvimento/shareposts/app/views/users/register.php
@@ -3,30 +3,85 @@
         <div class="col-md-6 mx-auto">
             <div class="card card-body bg-light mt-5">
                 <h2>Create An Account</h2>
-                <p>Please fill out this form to register with us</p>
-                <form action="<?php echo URLROOT; ?>/users/register" method="post">
-                    <div class="form-group">
-                        
-                        <!--NAME-->
+                <p>Please fill out this form to register with us</p>                               
+                <form action="<?php echo URLROOT; ?>/users/register" method="post">  
+                    
+                    <!--NAME-->
+                    <div class="form-group">   
                         <label 
                             for="name">Name: <sup>*</sup>
-                        </label>
+                        </label>                        
                         <input 
                             type="text" 
-                            neme="name" 
-                            class="
-                                form-control 
-                                form-control-lg 
-                                <?php echo(!empty($data['name_err'])) ? 'is-invalid' : ''; ?>
-                                "
+                            name="name" 
+                            class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" 
                             value="<?php echo $data['name'];?>"
                         >
-                        <span 
-                            class="invalid-feedback">
+                        <span class="invalid-feedback">
                             <?php echo $data['name_err']; ?>
                         </span>
-
                     </div>
+                   
+                
+                    
+                     <!--EMAIL-->
+                     <div class="form-group">   
+                        <label 
+                            for="email">Email: <sup>*</sup>
+                        </label>                        
+                        <input 
+                            type="text" 
+                            name="email" 
+                            class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>"                             
+                            value="<?php echo $data['email'];?>"
+                        >
+                        <span class="invalid-feedback">
+                            <?php echo $data['email_err']; ?>
+                        </span>
+                    </div>
+
+                     <!--PASSWORD-->
+                     <div class="form-group">   
+                        <label 
+                            for="password">Email: <sup>*</sup>
+                        </label>                        
+                        <input 
+                            type="password" 
+                            name="password" 
+                            class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>"                             
+                            value="<?php echo $data['password'];?>"
+                        >
+                        <span class="invalid-feedback">
+                            <?php echo $data['password_err']; ?>
+                        </span>
+                    </div>
+
+                     <!--CONFIRM PASSWORD-->
+                     <div class="form-group">   
+                        <label 
+                            for="confirm_password">Email: <sup>*</sup>
+                        </label>                        
+                        <input 
+                            type="password" 
+                            name="confirm_password" 
+                            class="form-control form-control-lg <?php echo (!empty($data['confirm_password'])) ? 'is-invalid' : ''; ?>"                             
+                            value="<?php echo $data['password'];?>"
+                        >
+                        <span class="invalid-feedback">
+                            <?php echo $data['confirm_password_err']; ?>
+                        </span>
+                    </div>
+
+                    <!--BUTTONS-->
+                    <div class="row">
+                        <div class="col">
+                            <input type="submit" value="Register" class="btn btn-success btn-block">                           
+                        </div>
+                        <div class="col">
+                            <a href="<?php echo URLROOT ;?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
+                        </div>
+                    </div>
+
                 </form>
             </div>
         </div>
