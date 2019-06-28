fa74661ad04753e2203e7455eefff0e02c34a5f2
Jeandrei <jeandreiwalter@gmail.com>
2019-06-28 13:13:18 -0300

    radio helpers

--- desenvolvimento/mvclogin/app/views/inc/navbar.php
+++ desenvolvimento/mvclogin/app/views/inc/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark md-3" style="margin-bottom:10px;">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark md-3" style="margin-bottom:10px;">
   <div class="container">
       <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
