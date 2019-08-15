de487c9a9a225a2d1d62f1ead2eed6c0b3065b2d
Jeandrei <jeandreiwalter@gmail.com>
2019-08-15 16:59:53 -0300

    atualizacao

--- desenvolvimento/mvclogin/bd/mvclogin_antigo.sql
+++ desenvolvimento/mvclogin/bd/mvclogin_antigo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 15/08/2019 às 19:59
--- Versão do servidor: 5.7.25
--- Versão do PHP: 7.2.14
+-- Generation Time: 13-Jun-2019 às 19:48
+-- Versão do servidor: 5.7.24
+-- versão do PHP: 7.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,48 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `mvclogin`
+-- Database: `shareposts`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `pessoas`
---
-
-CREATE TABLE `pessoas` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `sobrenome` varchar(50) NOT NULL,
-  `sexo` char(20) NOT NULL,
-  `cor` varchar(20) NOT NULL,
-  `telefone` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Despejando dados para a tabela `pessoas`
---
-
-INSERT INTO `pessoas` (`id`, `nome`, `sobrenome`, `sexo`, `cor`, `telefone`) VALUES
-(1, 'Jeandrei', 'Walter', 'Masculino', 'Branca', '991169267'),
-(2, 'Maricleia', 'Walter', 'Feminino', 'Branca', '991163863'),
-(3, 'Dexter', 'Walter', 'Masculino', 'Preto', '4545644'),
-(4, 'Biju', 'Walter', 'Feminino', 'Preta', '44548889'),
-(5, 'Beinha', 'Walter', 'Feminino', 'Marrom', '446588'),
-(6, 'Megui', 'Walter', 'Feminina', 'Preto', '4789789'),
-(7, 'Bibi', 'Walter', 'Feminina', 'Preto', '4547864654'),
-(8, 'Nina', 'Walter', 'Feminino', 'Branca', '454564512'),
-(9, 'Orlando', 'Walter', 'Masculino', 'Branco', '5665456454'),
-(10, 'Inez', 'Walter', 'Feminina', 'Branca', '44564464'),
-(11, 'Carla', 'Quadke', 'Feminina', 'Branca', '4654445'),
-(12, 'Patricia', 'Walerio', 'Feminina', 'Branca', '45446446'),
-(13, 'Fabio', 'Quadke', 'Masculino', 'Branco', '545646546'),
-(14, 'Gervasio', 'Walerio', 'Masculino', 'Branco', '5646546564');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `posts`
+-- Estrutura da tabela `posts`
 --
 
 CREATE TABLE `posts` (
@@ -72,7 +37,7 @@ CREATE TABLE `posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Despejando dados para a tabela `posts`
+-- Extraindo dados da tabela `posts`
 --
 
 INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `created_at`) VALUES
@@ -81,7 +46,7 @@ INSERT INTO `posts` (`id`, `user_id`, `title`, `body`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
@@ -93,7 +58,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Despejando dados para a tabela `users`
+-- Extraindo dados da tabela `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
@@ -102,45 +67,33 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
 (3, 'teste', 'jean.walter@penha.sc.gov.br', '$2y$10$EwxO3Gf78AQdSoVhVf6yxefdZFR2n3ON2w.t9XnyXsZPLJTNXfTGi', '2019-01-09 16:46:20');
 
 --
--- Índices de tabelas apagadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices de tabela `pessoas`
---
-ALTER TABLE `pessoas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices de tabela `posts`
+-- Indexes for table `posts`
 --
 ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `pessoas`
---
-ALTER TABLE `pessoas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT de tabela `posts`
+-- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de tabela `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
