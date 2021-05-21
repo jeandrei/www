e434d499056e5bf9f44565b2d109f20f9c5c9bf1
jeandrei <jeandreiwalter@gmail.com>
2021-05-21 10:51:39 -0300

    consulta protocolo responsivo

--- desenvolvimento/filaunica/app/views/filas/consultar.php
+++ desenvolvimento/filaunica/app/views/filas/consultar.php
@@ -1,7 +1,7 @@
 <?php require 'header.php'; ?>
 
 <div class="card">
-  <h5 class="card-header">Consulta do protocolo número: <b><?php echo $data->protocolo; ?> | Data de registro: <?php echo date('d/m/Y H:i:s', strtotime($data->registro));?></b></h5>
+  <h5 class="card-header">Consulta do protocolo número: <b><?php echo $data->protocolo; ?></b> | Data de registro: <b><?php echo date('d/m/Y H:i:s', strtotime($data->registro));?></b></h5>
   <div class="card-body">
     <h5 class="card-title">Posição na fila de espera: <b><?php echo $data->posicao; ?></b> para a etapa <b><?php echo $data->etapa; ?>.</b></h5>
     <p class="card-text">Status atual:<b> <?php echo $data->status; ?></b></p>
